3aecafc6c5f7accd39e0767716ffa6523090330c
Tommy Trøen <tommytroen@gmail.com>
2019-02-10 19:46:17 +0100

    remove ping

--- src/main/java/io/security/xacml/mock/MockPdpController.java
+++ src/main/java/io/security/xacml/mock/MockPdpController.java
@@ -4,7 +4,6 @@ import io.security.xacml.pep.PdpRequest;
 import io.security.xacml.pep.PdpResponse;
 import io.security.xacml.pep.attributes.StandardAttributes;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +15,6 @@ import java.util.Collections;
 public class MockPdpController {
 
     private static final String PERMIT_FOR_SUBJECT = "success";
-
-    @GetMapping("/ping")
-    public String ping(){
-        return "pong";
-    }
 
     @PostMapping("/authorize")
     public PdpResponse authorize(@RequestBody PdpRequest pdpRequest) {
